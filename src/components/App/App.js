7a9b103f92a353feabc1b84a947cf370432a5d0b
Arturo Chen <art.can.fly@gmail.com>
2020-09-04 22:52:42 -0400

    E5 done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
 import { GlobalStyles } from '../../GlobalStyles'
+import { ArtistRoute } from '../ArtistRoute'
+
+const DEFAULT_ARTIST_ID = '07YZf4WDAMNwqr4jfgOZ8y'
 
 const App = () => {
-  return ( <><GlobalStyles /><Router>TODO</Router></> )
+  return (
+    <Router>
+      <GlobalStyles />
+      <Switch>
+        <Route exact path='/'>
+          <Redirect path={ `/artists/${ DEFAULT_ARTIST_ID }` } />
+        </Route>
+        <Route path='/artists/:id'>
+          <ArtistRoute />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const ArtistRoute = () => {
+  return <div />
+}
+
+export default ArtistRoute
