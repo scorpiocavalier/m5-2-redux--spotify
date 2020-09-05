d61d5a49efab42aecdfa1b84754995ced9b9a38a
Arturo Chen <art.can.fly@gmail.com>
2020-09-04 21:49:34 -0400

    E3 done

--- server/server.js
+++ server/server.js
@@ -1,31 +1,43 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require( 'path' )
+const express = require( 'express' )
+const bodyParser = require( 'body-parser' )
+const fetch = require( 'isomorphic-fetch' )
 
-const app = new express();
-const port = 5678;
+const app = new express()
+const port = 5678
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
 
-app.get('/spotify_access_token', (req, res, next) => {
-  const clientId = process.env.SPOTIFY_CLIENT_ID;
-  const clientSecret = process.env.SPOTIFY_SECRET;
+app.get( '/spotify_access_token', async ( req, res, next ) => {
+  const clientId = process.env.SPOTIFY_CLIENT_ID
+  const clientSecret = process.env.SPOTIFY_SECRET
 
   // We need, annoyingly, a base64-encoded string of our id:secret, for spotify.
   // We can use Buffers to do this for us.
-  const authString = Buffer.from(clientId + ':' + clientSecret).toString(
+  const authString = Buffer.from( clientId + ':' + clientSecret ).toString(
     'base64'
-  );
+  )
 
-  // TODO: use authString in a request to Spotify!
-  res.send({ todo: true });
-});
+  const response = await fetch( 'https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${ authString }`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'grant_type=client_credentials'
+  } )
 
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error);
+  const data = await response.json()
+
+  return res.send(data)
+} )
+
+
+app.listen( port, function ( error ) {
+  if ( error ) {
+    console.error( error )
   } else {
-    console.info(`==> 🌎  Listening on port ${port}.`);
+    console.info( `==> 🌎  Listening on port ${ port }.` )
   }
-});
+} )
