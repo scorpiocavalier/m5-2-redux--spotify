143b3a84504075ca8d5767ec6cb4245e9c154577
Arturo Chen <art.can.fly@gmail.com>
2020-09-04 22:29:36 -0400

    E4 done

---
+++ src/reducers/auth-reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  token: null,
+  status: 'idle'
+}
+
+export default ( state = initialState, action ) => {
+  switch ( action.type ) {
+    default:
+      return state
+  }
+}
